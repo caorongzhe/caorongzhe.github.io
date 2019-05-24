2c17e9c25e00e8810d9567b23e83fca26d1719a4
caorongzhe <sniper.cao@outlook.com>
2019-05-24 13:08:56 +0800

    Site updated: 2019-05-24 13:08:49

---
+++ plugin/motto/motto.js
@@ -0,0 +1,114 @@
+var mingyan=new Array();
+mingyan.push("{'index':1,'content':'人情是纸张张薄，事事如棋局局新。  </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':2 ,'content':'贫在闹市无人问，富在深山有远亲。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':3 ,'content':'不信但看宴中酒，杯杯先敬富贵人。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':4 ,'content':'门前拴上高头马，不是亲来也是亲。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':5 ,'content':'门前放根讨饭棍，亲戚故友不上门。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':6 ,'content':'世上结交须黄金，黄金不多交不深。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':7 ,'content':'纵令然诺暂相许，终是悠悠路行人。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':8 ,'content':'有钱有酒多兄弟，急难何曽见一人。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':9 ,'content':'胜者为王败者寇，只重衣冠不重人。 </br> - </br>《增广贤文》'}");
+mingyan.push("{'index':10 ,'content':'三贫三富不到老，十年兴败多少人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':11 ,'content':'在官三日人问我，离官三日我问人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':12 ,'content':'古人不见今时月，今月曾经照古人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':13 ,'content':'近水楼台先得月，向阳花木易逢春。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':14 ,'content':'谁人背后无人说，谁人背后不说人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':15 ,'content':'百炼化身成铁汉，三缄其口学金人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':16 ,'content':'十分伶俐使七分，常留三分与儿孙。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':17 ,'content':'君子落得做君子，小人枉自做小人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':18 ,'content':'山中自有千年树，世上难逢百岁人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':19 ,'content':'岂无远道思亲泪，不及高堂念子心。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':20 ,'content':'堂上二老是活佛，何用灵山朝世尊。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':21 ,'content':'平身不做皱眉事，世上应无切齿人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':22 ,'content':'越奸越狡越贫穷，奸狡原来天不容。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':23 ,'content':'富贵若从奸狡起，世间呆汉喝西风。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':24 ,'content':'求人须求大丈夫，济人须济急时无。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':25 ,'content':'茫茫四海人无数，哪个男儿是丈夫。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':26 ,'content':'人情是水分高下，世事如云任卷舒。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':27 ,'content':'如山不怕伤人虎，只怕人情两面刀。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':28 ,'content':'务求到处人情好，不饮随他酒价高。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':29 ,'content':'知事少时烦恼少，识人多处是非多。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':30 ,'content':'秋至满山皆秀色，春来无处不花香。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':31 ,'content':'贫无达士将金赠，病有高人说药方。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':32 ,'content':'美人卖笑千金易，壮士穷途一饭难。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':33 ,'content':'少时总觉为人易，华年方知立业难。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':34 ,'content':'用心计较般般错，退迟思量事事难。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':35 ,'content':'世上闲愁千万斛，不叫一点上眉端。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':36 ,'content':'毁身每是作恶日，成名皆在行善时。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':37 ,'content':'莫把真心空计较，唯有大德享万年。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':38 ,'content':'谗言败坏真君子，美色消磨狂少年。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':39 ,'content':'神仙难断阴骘事，黄天不昧苦心人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':40 ,'content':'人恶人怕天不怕，人善人欺天不欺。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':41 ,'content':'善恶到头终有报，只争来早与来迟。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':42 ,'content':'平身不做亏心事，半夜敲门心不惊。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':43 ,'content':'万事劝人休瞒昧，举头三尺有神明。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':44 ,'content':'鬼神可敬不可谄，冤家宜解不宜结。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':45 ,'content':'人生何处不相逢，莫因小怨动声色。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':46 ,'content':'好义固为人所钦，贪利乃为鬼所笑。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':47 ,'content':'贤者不炫己之长，君子不夺人所好。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':48 ,'content':'善业可为须着力，是非闲杂莫劳心。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':49 ,'content':'良田不由心田置，产业变为冤业折。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':50 ,'content':'千年天地八百主，田是主人人是客。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':51 ,'content':'阴地不如新地好，命运在人不在天。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':52 ,'content':'有理问得君王倒，有钱难买子孙贤。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':53 ,'content':'水暖水寒鱼自知，花开花谢春不管。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':54 ,'content':'蜗牛角上校雌雄，石光火中争长短。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':55 ,'content':'留心学到古人难，立脚怕随流水转。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':56 ,'content':'少而寡欲颜色好，老不求官梦也闲。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':57 ,'content':'妙药难医冤孽病，横财不富命穷人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':58 ,'content':'多情自古空遗恨，好梦由来最易醒。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':59 ,'content':'命中只有八合米，走进天下难满升。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':60 ,'content':'知恩报恩天下少，反面无情世间多。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':61 ,'content':'荣宠旁边辱等待，贫贱背后福跟随。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':62 ,'content':'各人生死各人了，管人闲事受人磨。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':63 ,'content':'易涨易退山溪水，易反易覆小人心。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':64 ,'content':'红粉佳人休说老，风流浪子莫叫贫。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':65 ,'content':'龙游浅水遭虾戏，虎落平阳被犬欺。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':66 ,'content':'得食猫儿强似虎，褪毛鸾凤不如鸡。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':67 ,'content':'为人莫作千年计，三十河东四十西。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':68 ,'content':'当路莫栽荆棘树，他年免挂子孙衣。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':69 ,'content':'屋漏偏遭连阴雨，船慢又遇顶头风。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':70 ,'content':'记得少年骑竹马，看看又是白头翁。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':71 ,'content':'美人绝色原妖物，乱世多财是祸根。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':72 ,'content':'畜生易度人难度，宁度畜生不度人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':73 ,'content':'天作棋盘星作子，水有源头树有根。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':74 ,'content':'昨日花开今日谢，百年人有万年心。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':75 ,'content':'北邙荒冢无贫富，云垒浮云变古今。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':76 ,'content':'世事茫茫难自料，清风明月冷看人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':77 ,'content':'守口不谈新旧事，知心难得两三人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':78 ,'content':'欲知世情须尝胆，会尽人情暗点头。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':79 ,'content':'是非只为多开口，烦恼皆为强出头。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':80 ,'content':'人生七十古来稀，问君还有几春秋。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':81 ,'content':'一家之计在于和，一生之计在于勤。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':82 ,'content':'一朝天子一朝臣，一辈新鲜一辈陈。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':83 ,'content':'一苗露水一苗草，一层山水一层人。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':84 ,'content':'闷坐书馆闲操心，看来全是论古今。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':85 ,'content':'书中有真就有假，世人认假不认真。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':86 ,'content':'假作真时真亦假，真作假时假也真。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':87 ,'content':'真真假假难分解，假者自假真自真。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':88 ,'content':'回忆事情般般假，借假修真破迷津。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':90 ,'content':'由来富贵三更梦，何必楚楚苦用心。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':91 ,'content':'识破人情惊破胆，看穿世间心胆寒。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':92 ,'content':'白璧易埋千古恨，黄金难买一身闲。</br> - </br>《增广贤文》'}");
+mingyan.push("{'index':93 ,'content':'人生究竟归何处，看破放下随万缘。</br> - </br>《增广贤文》'}");
+
+/**
+    随机获取名言.
+
+    直接调用此函数即可.
+*/
+function getMingYan(){
+  return (mingyan[parseInt(Math.random()*(mingyan.length-1)+1)]);
+}
+
+function getItem(){
+  return eval('(' +(mingyan[parseInt(Math.random()*(mingyan.length-1)+1)])+ ')');
+}
+
+/*
+  只获取内容.
+*/
+function getMingYanContent(){
+  var item = getItem();
+  return item['content'];
+}
